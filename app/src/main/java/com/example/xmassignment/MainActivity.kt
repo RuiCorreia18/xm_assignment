e68aeed1db0f9bf370f57c0a8b3096fefd10bc6b
Rui Correia <rui-manuel-18@hotmail.com>
2024-06-03 17:31:22 +0100

    Start page

--- app/src/main/java/com/example/xmassignment/MainActivity.kt
+++ app/src/main/java/com/example/xmassignment/MainActivity.kt
@@ -3,27 +3,45 @@ package com.example.xmassignment
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.xmassignment.Destinations.QUESTION_PAGE
+import com.example.xmassignment.Destinations.START_PAGE
+import com.example.xmassignment.ui.questions.QuestionsScreen
 import com.example.xmassignment.ui.theme.XMAssignmentTheme
+
+
+object Destinations {
+    const val START_PAGE = "start"
+    const val QUESTION_PAGE = "question"
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
+            val navController = rememberNavController()
             XMAssignmentTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface {
+                    NavHost(navController = navController, startDestination = START_PAGE) {
+                        composable(START_PAGE) {
+                            InitialScreen(navController)
+                        }
+                        composable(QUESTION_PAGE) {
+                            QuestionsScreen()
+                        }
+                    }
                 }
             }
         }
@@ -31,17 +49,16 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    XMAssignmentTheme {
-        Greeting("Android")
+fun InitialScreen(navController: NavHostController) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Button(
+            onClick = { navController.navigate("questions_screen") }
+        ) {
+            Text(text = "Start Survey")
+        }
     }
 }
